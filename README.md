cf369dcbc5ee5124ee6ff98ec49ec0f4c635a838
Rafał Janiczek <rafal.janiczek@int.pl>
2024-11-30 10:56:16 +0100

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2024
+
+These are my solutions using PHP 8.x.
+
+Just run `php 01.php`
---
+++ template.php
@@ -0,0 +1,24 @@
+<?php
+
+ini_set('memory_limit', '4G');
+
+function _display(int $part): void
+{
+    global $argv;
+    $day = basename($argv[0], '.php');
+
+    $startMem = memory_get_peak_usage();
+    $startTs = hrtime(true);
+    $func = 'part' . $part;
+
+    echo "== Day $day part $part ==" . PHP_EOL;
+    echo 'Result: ' . $func(input(rtrim(file_get_contents(__DIR__ . "/inputs/$day.txt")))) . PHP_EOL;
+    echo 'Time  : ' . sprintf('%.6f', (hrtime(true) - $startTs) / 1e+9) . ' s' . PHP_EOL;
+    echo 'Memory: ' . sprintf('%.5f', ((memory_get_peak_usage() - $startMem) / 1024 / 1024)) . ' MB' . PHP_EOL;
+    echo PHP_EOL;
+}
+
+/** @uses part1 */
+_display(1);
+/** @uses part2 */
+_display(2);
